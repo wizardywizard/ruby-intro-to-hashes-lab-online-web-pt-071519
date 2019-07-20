4fe630a8d638ac901fa5234de009b05f1b332be9
David Little <littledavid175@gmail.com>
2019-07-20 21:29:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,19 +42,7 @@ def monopoly_with_third_tier
 	      :two_pieces_owned => 50, 
 	      :three_pieces_owned => 100, 
 	      :four_pieces_owned => 200
-	    },
-	  {
-	    :pieces => 4,
-	    :names => {
-	      
-	    },
-	    :rent_in_dollars => {
-	    :one_piece_owned => 25,
-	    :two_pieces_owned => 50,
-	    :three_pieces_owned => 100
-	    :four_pieces_owned => 200
-	  },
-	  }
+	    }
 	  }
 	}
 end
