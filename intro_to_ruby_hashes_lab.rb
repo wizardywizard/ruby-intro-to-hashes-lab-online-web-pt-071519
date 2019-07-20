9dda1b48e9b979af167227784d5d6a54a3508c81
David Little <littledavid175@gmail.com>
2019-07-20 21:26:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,8 +7,14 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {
-	  :railroads =>
-	  {:pieces => 4, :names => {}, :rent_in_dollars =>{}
+	  :railroads => {
+	    :pieces => 4,
+	    :names => {
+	      
+	    },
+	    :rent_in_dollars =>{
+	      
+	    }
 	  }
 	}
 end
